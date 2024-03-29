107db2b412697f9da87462b12804243f4e240500
Ivan Jelisavcic <ivanjelisavcic6@gmail.com>
2024-03-29 09:31:14 +0100

    Prvi komit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'quotes',
+    loadChildren: () => import('./quotes/quotes.module').then( m => m.QuotesPageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'quotes',
     pathMatch: 'full'
   },
 ];
