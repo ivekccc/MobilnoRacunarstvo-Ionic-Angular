aa97036497f234564d952b281ef3492a638d7d3f
Ivan Jelisavcic <ivanjelisavcic6@gmail.com>
2024-03-29 09:44:42 +0100

    Page lifecycle

--- src/app/fun-facts/fun-facts.page.ts
+++ src/app/fun-facts/fun-facts.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fun-facts',
   templateUrl: './fun-facts.page.html',
   styleUrls: ['./fun-facts.page.scss'],
 })
-export class FunFactsPage implements OnInit {
+export class FunFactsPage implements OnInit,OnDestroy {
 
-  constructor() { }
+  constructor() {
+    console.log('constructor');
+   }
 
   ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ionViewWillEnter(){
+    console.log('ionViewWillEnter');
+  }
+
+  ionViewDidEnter(){
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewWillLeave(){
+    console.log('ionViewWillLeave');
+  }
+
+  ionViewDidLeave(){
+    console.log('ionViewDidLeave');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
   }
 
 }
--- src/app/quotes/quotes.page.ts
+++ src/app/quotes/quotes.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.page.html',
   styleUrls: ['./quotes.page.scss'],
 })
-export class QuotesPage implements OnInit {
+export class QuotesPage implements OnInit,OnDestroy {
 
-  constructor() { }
+  constructor() {
+    console.log('constructor');
+   }
 
   ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ionViewWillEnter(){
+    console.log('ionViewWillEnter');
+  }
+
+  ionViewDidEnter(){
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewWillLeave(){
+    console.log('ionViewWillLeave');
+  }
+
+  ionViewDidLeave(){
+    console.log('ionViewDidLeave');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
   }
 
 }
